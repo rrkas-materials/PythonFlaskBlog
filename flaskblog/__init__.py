2ec7eccad3c583c89f7befbba35af3cb9eec5696
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-17 16:54:44 +0530

    error handling

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -26,9 +26,11 @@ def create_app(config_class=Config):
     from .users.routes import users
     from .posts.routes import posts
     from .main.routes import main
+    from .errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
