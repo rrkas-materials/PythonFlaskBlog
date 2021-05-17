d2f4835a35717a07756508cbc3cf0e2b006a4896
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-17 19:19:04 +0530

    db create debugged

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -32,5 +33,8 @@ def create_app(config_class=Config):
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+
+    if os.path.exists(os.path.join(app.root_path, 'site.db')):
+        db.create_all()
 
     return app
