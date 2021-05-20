88cc82122e1a3a4e9237d9112c60f605537c5557
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-20 20:15:26 +0530

    minor improvements

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
     SECRET_KEY = '1ee79fcbd2bb2730153928946a4ec1fb'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'flaskblog.rrkas@gmail.com'  # TODO: change to your email
-    MAIL_PASSWORD = 'BLOGrrkasFLASK!'  # TODO: change to your password
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # TODO: change to your email
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # TODO: change to your password
